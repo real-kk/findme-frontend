c57e2a0ceef3ae728890b73bc467b2459480dafe
KIMDOHYUN <kdh5998@ajou.ac.kr>
2020-10-19 19:16:06 +0900

    axios 설치

---
+++ find_me/src/Tab/Home.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StyleSheet,  View, Text, Button, TextInput } from 'react-native';
+
+class TabUserScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>UserScreen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default TabUserScreen;
